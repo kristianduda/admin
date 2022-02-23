aa00e8e395486ccebba9a7857d6dec4ed1fa4b85
Kristian Duda <kristianduda83@gmail.com>
2022-02-23 16:14:50 +0100

    authUtils: refactoring

--- src/utils/authUtils.js
+++ src/utils/authUtils.js
@@ -1,13 +1,7 @@
 import { auth as a } from "kd-web";
 
-export const auth = (username, password) => {
-  return a.auth(username, password);
-};
+export const auth = (username, password) => a.auth(username, password)
 
-export const updateUser = (data) => {
-  return a.updateUser(data._id, data);
-};
+export const updateUser = (data) => a.updateUser(data._id, data);
 
-export const getUsers = (filters, sort, page, search) => {
-  return a.getUsers(filters, sort, page, search);
-}
+export const getUsers = (filters, sort, page, search) => a.getUsers(filters, sort, page, search);
